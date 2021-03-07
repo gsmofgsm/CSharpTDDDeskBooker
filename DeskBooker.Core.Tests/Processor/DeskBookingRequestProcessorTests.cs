3e04cae6e882a337fe6e38a4881331afc62b5783
Qing Ma <gsmofgsm@hotmail.com>
2021-03-07 22:27:29 +0100

    First Test

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Thomas",
+                LastName = "Huber",
+                Email = "thomas@thomasclaudiushuber.com",
+                Date = new DateTime(2020, 1, 28)
+            };
+
+            var processor = new DeskingBookingRequestProcessor();
+
+            // Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
---
+++ DeskBooker.Core.Tests/Processor/DeskingBookingRequestProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskingBookingRequestProcessor
+    {
+        public DeskingBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            return new DeskBookingResult 
+            { 
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date,
+            };
+        }
+    }
+}
