847b508978dbfdb674c6833fa08bd7c525c40f96
Qing Ma <q.ma@ncontrol.cloud>
2021-03-11 21:16:24 +0100

    BookDeskConfirmation Page

---
+++ DeskBooker.Web.Tests/Pages/BookDeskConfirmationModelTests.cs
@@ -0,0 +1,32 @@
+﻿using DeskBooker.Web.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeskBooker.Web.Tests.Pages
+{
+    public class BookDeskConfirmationModelTests
+    {
+        [Fact]
+        public void ShouldStoreParameterValuesInProperties()
+        {
+            // Arrange
+            const int deskBookingId = 7;
+            const string firstName = "Thomas";
+            var date = new DateTime(2020, 1, 28);
+
+            var bookDeskConfirmationModel = new BookDeskConfirmationModel();
+
+            // Act
+            bookDeskConfirmationModel.OnGet(deskBookingId, firstName, date);
+
+            // Assert
+            Assert.Equal(deskBookingId, bookDeskConfirmationModel.DeskBookingId);
+            Assert.Equal(firstName, bookDeskConfirmationModel.FirstName);
+            Assert.Equal(date, bookDeskConfirmationModel.Date);
+        }
+    }
+}
